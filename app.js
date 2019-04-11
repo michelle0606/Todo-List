705829fd89bdf6868df55d480e2049e68a65442e
walle <michelle210181@gmail.com>
2019-04-11 10:06:25 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1/todo", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world!!!!!!");
+});
+
+app.listen(3000, () => {
+  console.log("john legend !!!!!!!");
+});
