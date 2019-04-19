04021ed7c6bdfce7dca0872b2f965b49669be050
walle <michelle210181@gmail.com>
2019-04-20 00:11:26 +0800

    Add user login feature using passport and express-session

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("passport");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+// 使用 express session
+app.use(
+  session({
+    secret: "90490jq2jdncjrkljfilkcmoiqw3902i0dokl"
+  })
+);
+// 使用 Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// 載入 Passport config
+require("./config/passport")(passport);
+// 登入後可以取得使用者的資訊方便我們在 view 裡面直接使用
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 mongoose.connect("mongodb://127.0.0.1/todo", { useNewUrlParser: true });
 
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user-model");
+const passport = require("passport");
 
 // 登入頁面
 router.get("/login", (req, res) => {
@@ -8,13 +9,11 @@ router.get("/login", (req, res) => {
 });
 
 // 登入檢查
-router.post("/login", (req, res) => {
-  const user = User(req.body);
-
-  User.save(err => {
-    if (err) return console.log(err);
-    return res.redirect("/");
-  });
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/", // 登入成功會回到根目錄
+    failureRedirect: "/users/login" // 失敗會留在原本頁面
+  })(req, res, next);
 });
 
 // 註冊頁面
